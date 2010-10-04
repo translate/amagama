e1263fa4228ee76d6708f2ad19d0d3b1a1e8a18e
Alaa Abd el Fattah <alaa@translate.org.za>
2010-10-04 23:23:36 +0000

    use Flask-script to implement database initializing and importing translation files into tmdb
    svn path=/src/trunk/amagama/; revision=15960

---
+++ amagama/commands.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Zuza Software Foundation
+#
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import sys
+import os
+
+from translate.storage import factory
+
+from flask import current_app
+
+from flaskext.script import Command, Option
+
+class InitDB(Command):
+    """create database tables"""
+    def run(self):
+        current_app.tmdb.init_db()
+
+class BuildTMDB(Command):
+    """Populate Translation Memory database from bilinugual translation files"""
+
+    option_list = (
+        Option('--source-language', '-s', dest='slang'),
+        Option('--target-language', '-t', dest='tlang'),
+        Option('--input', '-i', dest='filename'),
+    )
+
+    def run(self, slang, tlang, filename):
+        self.source_lang = slang
+        self.target_lang = tlang
+
+        if not os.path.exists(filename):
+            print >> sys.stderr, "cannot process %s: does not exist" % filename
+        elif os.path.isdir(filename):
+            self.handledir(filename)
+        else:
+            self.handlefile(filename)
+        current_app.tmdb.connection.commit()
+
+    def handlefile(self, filename):
+        try:
+            store = factory.getobject(filename)
+        except Exception, e:
+            print >> sys.stderr, str(e)
+            return
+        # do something useful with the store and db
+        try:
+            current_app.tmdb.add_store(store, self.source_lang, self.target_lang, commit=False)
+        except Exception, e:
+            print e
+            raise
+        print "File added:", filename
+
+    def handlefiles(self, dirname, filenames):
+        for filename in filenames:
+            pathname = os.path.join(dirname, filename)
+            if os.path.isdir(pathname):
+                self.handledir(pathname)
+            else:
+                self.handlefile(pathname)
+
+    def handledir(self, dirname):
+        path, name = os.path.split(dirname)
+        if name in ["CVS", ".svn", "_darcs", ".git", ".hg", ".bzr"]:
+            return
+        entries = os.listdir(dirname)
+        self.handlefiles(dirname, entries)
+ 
