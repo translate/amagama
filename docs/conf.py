4a270d5b952da9615d13acd6b93c1e0856bc238e
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-05-12 13:00:10 +0200

    Docs: changed conf.py to use amaGama everywhere and updated copyright dates
    [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Amagama'
-copyright = u'2012, Translate.org.za'
+project = u'amaGama'
+copyright = u'2012, 2013, Translate.org.za'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -190,7 +190,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual])
 latex_documents = [
-  ('index', 'Amagama.tex', u'Amagama Documentation',
+  ('index', 'Amagama.tex', u'amaGama Documentation',
    u'Translate.org.za', 'manual'),
 ]
 
@@ -220,7 +220,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'amagama', u'Amagama Documentation',
+    ('index', 'amagama', u'amaGama Documentation',
      [u'Translate.org.za'], 1)
 ]
 
@@ -234,8 +234,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Amagama', u'Amagama Documentation',
-   u'Translate.org.za', 'Amagama', 'Translation Memory server.',
+  ('index', 'amagama', u'amaGama Documentation',
+   u'Translate.org.za', 'amaGama', 'Translation Memory server.',
    'Miscellaneous'),
 ]
 
