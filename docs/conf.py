170c072f8f13cf6a7213d90c0ab99f89b4dfcc1e
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-02-27 20:00:41 +0100

    Docs: Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'amaGama'
-copyright = u'2012, 2013, Translate.org.za'
+copyright = u'2012-2014, Translate.org.za'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
