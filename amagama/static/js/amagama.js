240097f8a3c71df340c01c5240bc9ab0880e828a
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-03-31 14:47:32 +0200

    Web UI: Use namespace to avoid polluting the global JavaScript namespace

--- amagama/static/js/amagama.js
+++ amagama/static/js/amagama.js
@@ -1,71 +1,82 @@
-$(document).on('submit', '#js-amagama-form', doSearch);
+(function ($) {
 
+  window.MGM = window.MGM || {};
 
-function doSearch (event) {
-  event.preventDefault();
+  MGM.search = {
 
-  var searchTerms = $('#js-search-box').val();
+    init: function () {
+      $(document).on('submit', '#js-amagama-form', MGM.search.doSearch);
+    },
 
-  // Only trigger the search if query terms have been provided.
-  if (searchTerms) {
-    //TODO look for a way to specify min_similarity and max_candidates on the
-    // search box instead of hardcoding them here.
-    var minSimilarity = 30,
-        maxCandidates = 20,
-        sourceLanguage = $('#js-source-language').val(),
-        targetLanguage = $('#js-target-language').val();
+    doSearch: function (event) {
+      event.preventDefault();
 
-    var URLParams = $.param({
-      'source': searchTerms,
-      'min_similarity': minSimilarity,
-      'max_candidates': maxCandidates
-    });
+      var searchTerms = $('#js-search-box').val();
 
-    var queryURL = [
-      '/tmserver/', sourceLanguage, '/', targetLanguage, '/unit/?', URLParams
-    ].join('');
+      // Only trigger the search if query terms have been provided.
+      if (searchTerms) {
+        //TODO look for a way to specify min_similarity and max_candidates on
+        // the search box instead of hardcoding them here.
+        var minSimilarity = 30,
+            maxCandidates = 20,
+            sourceLanguage = $('#js-source-language').val(),
+            targetLanguage = $('#js-target-language').val();
 
-    // Query the amaGama API and display the results.
-    $.getJSON(queryURL, function (data) {
-      $('#js-similar-title').text([
-        searchTerms, ' (', sourceLanguage, ' → ', targetLanguage, ')'
-      ].join(''));
+        var URLParams = $.param({
+          'source': searchTerms,
+          'min_similarity': minSimilarity,
+          'max_candidates': maxCandidates
+        });
 
-      $('#js-similar-count').text('Found ' + data.length + ' results.');
+        var queryURL = [
+          '/tmserver/', sourceLanguage, '/', targetLanguage, '/unit/?',
+          URLParams
+        ].join('');
 
-      if (data.length) {
-        var currentResult,
-            resultQuality,
-            resultsHTML = [];
+        // Query the amaGama API and display the results.
+        $.getJSON(queryURL, function (data) {
+          $('#js-similar-title').text([
+            searchTerms, ' (', sourceLanguage, ' → ', targetLanguage, ')'
+          ].join(''));
 
-        for (var i = 0; i < data.length; i++) {
-          currentResult = data[i];
+          $('#js-similar-count').text('Found ' + data.length + ' results.');
 
-          resultsHTML.push('<tr><td>');
-          resultsHTML.push(currentResult.source);
-          resultsHTML.push('</td><td>');
-          resultsHTML.push(currentResult.target);
-          resultsHTML.push('</td><td>');
+          if (data.length) {
+            var currentResult,
+                resultQuality,
+                resultsHTML = [];
 
-          resultQuality = currentResult.quality.toFixed(2);
-          if (currentResult.quality === 100.00) {
-            resultsHTML.push('<span class="exact-match">');
-            resultsHTML.push(resultQuality);
-            resultsHTML.push('</span>');
+            for (var i = 0; i < data.length; i++) {
+              currentResult = data[i];
+
+              resultsHTML.push('<tr><td>');
+              resultsHTML.push(currentResult.source);
+              resultsHTML.push('</td><td>');
+              resultsHTML.push(currentResult.target);
+              resultsHTML.push('</td><td>');
+
+              resultQuality = currentResult.quality.toFixed(2);
+              if (currentResult.quality === 100.00) {
+                resultsHTML.push('<span class="exact-match">');
+                resultsHTML.push(resultQuality);
+                resultsHTML.push('</span>');
+              }
+              else {
+                resultsHTML.push(resultQuality);
+              }
+              resultsHTML.push('</td><td>');
+              resultsHTML.push(currentResult.rank.toFixed(2));
+              resultsHTML.push('</td></tr>');
+            }
+
+            $('#js-similar-results').html(resultsHTML.join(''));
+            $('#js-similar-table').show();
+          } else {
+            $('#js-similar-table').hide();
           }
-          else {
-            resultsHTML.push(resultQuality);
-          }
-          resultsHTML.push('</td><td>');
-          resultsHTML.push(currentResult.rank.toFixed(2));
-          resultsHTML.push('</td></tr>');
-        }
-
-        $('#js-similar-results').html(resultsHTML.join(''));
-        $('#js-similar-table').show();
-      } else {
-        $('#js-similar-table').hide();
+        });
       }
-    });
-  }
-};
+    }
+  };
+
+}(jQuery));
